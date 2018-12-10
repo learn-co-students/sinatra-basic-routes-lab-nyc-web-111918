d1c401ddb828e0bf59244a1c32a8cc7a0a2eb9b5
lumrachele <rlum894@gmail.com>
2018-12-10 09:47:19 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  get '/name' do
+    "My name is Rachel."
+  end
+  get '/hometown' do
+    "My hometown is Denville, NJ."
+  end
+  get '/favorite-song' do
+    "My favorite song is 'Guap' by Yaeji."
+  end
 end
