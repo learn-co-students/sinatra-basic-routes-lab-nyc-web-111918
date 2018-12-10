fc4e04e43ac8b03e47a15ccdf748ef8e4a942b8a
Tonyjlum <tonyjlum@gmail.com>
2018-12-10 13:12:12 -0500

    2004 Facebook

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path =~ /name/
+      resp.write "My name is Tony"
+    elsif req.path =~ /hometown/
+      resp.write "My hometown is Queens"
+    elsif req.path =~ /favorite-song/
+      resp.write "My favorite song is The Smashing Pumpkins - Perfect"
+    else# not needed for the lab.
+      resp.write "Route Not Found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+
+
 end
