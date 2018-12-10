f5808665a095f2920d467ebfbd817f65aa39160a
Damon Collins <damon.c.p.collins@gmail.com>
2018-12-10 10:00:35 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Damon Collins"
+  end
+
+  get '/hometown' do
+    "My hometown is Queens, NY"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is a song"
+  end
 end
